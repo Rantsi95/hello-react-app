3b9a0cc23dffd96da00e12ddeb85b542c9523319
Rantsi95 <juha.rantajarvi@ohjelmointikielikylpy.fi>
2020-10-13 16:57:15 +0300

    Implements deletion of tasks

--- hello-reactapp/src/App.js
+++ hello-reactapp/src/App.js
@@ -2,16 +2,35 @@ import React, {useEffect, useState } from "react";
 import './App.css';
 
 function App() {
-  const [taskList, setTaskList] = useState(["juu", "jaa", "joo"])
-  const [newTaskItem, updateNewTaskItem] = useState("")  
+  const [taskList, setTaskList] = useState([])
+  const [newTaskItem, updateNewTaskItem] = useState("")
+  
+  
 
   let addNewTaskToList = function(taskItem) {
       let taskListCopy = [...taskList]
-      taskListCopy.push(taskItem)
+      let taskItemObj = {"id": Math.floor(Date.now() / 1000), "taskContent": taskItem }
+      taskListCopy.push(taskItemObj)
       setTaskList(taskListCopy)
+      updateNewTaskItem("")
 
     }
 
+
+    let deleteTask = function(taskId) {
+        let taskListCopy = [...taskList]
+        // taskListCopy.forEach((taskListItem) => if(taskListItem.id === taskId {taskListAnotherCopy.push(taskListItem)})
+        let shortenedTaskList = taskListCopy.filter(taskListItem => taskListItem.id !== taskId)
+        // console.log(shortenedTaskList)
+        setTaskList(shortenedTaskList)
+    
+      }
+
+
+    let deleteTaskFromList = function(taskItemId) {
+
+
+    }
 
   return (
     <div className="App">
@@ -19,8 +38,8 @@ function App() {
     <input type="text" value={newTaskItem} onChange={ (e) => updateNewTaskItem(e.target.value)}/>
     <input type="button" value="Lisää taski" onClick= {() => addNewTaskToList(newTaskItem) } />
 
-      {taskList.map( (singleTask)  => <p>{singleTask}</p>)}
-      
+      {taskList.map( (singleTask)  => <p>{singleTask.taskContent}<button onClick={() => deleteTask(singleTask.id)}>X</button></p>)}
+
      {/* <button type = "button" onClick={() => increaseCounter(counter + 1)}>Laskurin arvo {counter}</button> */}
     </div>
   );
